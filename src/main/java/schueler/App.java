c00f9ec7ef303a5b00e137af7d152e82e7c24843
Patrick <jenkner.patrick@mm-bbs.de>
2025-01-09 10:37:11 +0100

    add test to App.java

--- src/main/java/schueler/App.java
+++ src/main/java/schueler/App.java
@@ -15,11 +15,22 @@ public class App {
             SQLiteConfig config = new SQLiteConfig();
             config.enforceForeignKeys(true);
             Connection c = DriverManager.getConnection("jdbc:sqlite:todo_klein_org.db", config.toProperties());
+
+            Priority p = new Priority(5,"mittel wichtig");
+            
             Statement st = c.createStatement();
-            ResultSet rs = st.executeQuery("select * from priority");
+            st.execute(p.getCreateStatement());
+            ResultSet rs = st.executeQuery("select * from priority order by id DESC");
             while (rs.next()) {
                 System.out.println(rs.getInt(1) + " " + rs.getInt(2) + " " + rs.getString(3));
             }
+            /*
+            while (rs.next()) {
+                p.setId(rs.getInt(1));
+            }
+            p.setValue(10);
+            st.execute(p.getUpdateStatement());
+            st.execute(p.getDeleteStatement());*/
         } catch (ClassNotFoundException e) {
             System.out.println("Treiber nicht gefunden");
             e.printStackTrace();
